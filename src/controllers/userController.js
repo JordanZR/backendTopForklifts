2ad5e1396dcaa9c1285a89d475eeea0a9607ae0c
JordanZ <jordan.zelayahs@gmail.com>
2023-05-05 13:59:20 -0600

    Added getUsuario to check the login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,6 +10,14 @@ usuarioCtrl.getUsuarios = (req, res)=>{
         res.json(result)
     })
 }
+usuarioCtrl.getUsuario = (req, res)=>{
+    query = "SELECT * from usuario WHERE id = " + body.req.id
+    connection.query(query, function (err, result) {
+        if (err) console.log(err)
+        res.json(result)
+    })
+}
+
 
 usuarioCtrl.postUsuario = (req, res)=>{
     query = "INSERT INTO usuario(nombre, apellido, correo, password_, tipo) VALUES('" + req.body.nombre +  "','" + req.body.apellido + "','" + req.body.correo + "','" + req.body.password_ + "','" + req.body.tipo + "')"
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,7 +2,8 @@ var app = require('../index')
 
 const usuarioCtrl = require('../controllers/userController')
 
-app.get('/usuario', usuarioCtrl.getUsuarios)
+app.get('/usuario', usuarioCtrl.getUsuario)
+app.get('/usuarios', usuarioCtrl.getUsuarios)
 app.post('/usuario', usuarioCtrl.postUsuario)
 app.put('/usuario', usuarioCtrl.updateUsuario)
 app.delete('/usuario', usuarioCtrl.deleteUsuario)
