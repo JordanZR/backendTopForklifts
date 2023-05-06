633be3d24c554b77f06038de45f1805e89d5265d
JordanZ <jordan.zelayahs@gmail.com>
2023-05-06 14:14:24 -0600

    Fixed issues

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -5,7 +5,49 @@ const cliente = {}
 
 email.send =(req, res)=>{
     
-    //Checking if Cliente exists
+    //Email sender
+    try{
+        //Titan email
+
+        let transporter = nodemailer.createTransport({
+            host: "smtp.titan.email",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "jordan.zelaya@straydog.cloud",
+                pass: "Ocita1Vacilante2@",
+            },
+        });
+
+        /*Verify transporter
+        transporter.verify(function(error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Server is ready to take our messages');
+            }
+        });
+        */
+
+        let mailOptions = {
+            from: 'jordan.zelaya@straydog.cloud', // sender address
+            to: [
+                req.body.correo,
+                'jordan.zelayahs@gmail.com'
+            ],
+            subject: 'Cotización', // Subject line
+            text: req.body.nombre + " " + req.body.apellido + " de la empresa: " + req.body.empresa + " desea saber lo siguiente: " + req.body.contenido + ". Su número de telefono es: " + req.body.telefono, // plain text body
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+
+        });
+
+        //Checking if Cliente exists
     query = "SELECT * from cliente WHERE correo = '" + req.body.correo + "'"
     connection.query(query, function (err, result) {
         if (err) console.log(err)
@@ -44,6 +86,11 @@ email.send =(req, res)=>{
             
         }
     })
+    }catch (err){
+        console.log("Error")
+    }
+
+    
 }
 
 module.exports = email
